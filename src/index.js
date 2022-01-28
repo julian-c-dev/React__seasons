97ea0a2b1f9c8392ad94ecbaef7565907da6af42
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-01-28 19:17:27 +0000

    Added Spinner comp

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
-
+  // ^ lifecycle method
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
       (err) => this.setState({ errorMessage: err.message })
     );
   }
-
-  // React says we have to define render!!
-  render() {
+  // ^ helper function => avoding so much logic inside the render method
+  // ^ also allow us to wrap our elements althoger no matter which one will be displayed
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage} </div>;
     }
@@ -21,7 +22,16 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <Spinner message="Please, accept location request" />
+      </div>
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
